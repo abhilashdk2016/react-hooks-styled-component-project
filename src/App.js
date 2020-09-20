d68cab8a128f48dae20a8442da4b1370ac62d4f7
abhilashdk2016 <abhilashdk2014@outlook.com>
2020-09-20 23:39:36 +0530

    Global Styles & Google Fonts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import './App.css';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`;
 
 function App() {
   return (
-    <div>
-      react-hooks-styled-component-project
-    </div>
+    <>
+      <GlobalStyle />
+      <div>
+        react-hooks-styled-component-project
+      </div>
+    </>
+    
   );
 }
 
