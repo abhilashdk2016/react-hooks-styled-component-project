407d218232ed6407e934b4aa8c94f69bdf86a2cb
abhilashdk2016 <abhilashdk2014@outlook.com>
2020-09-22 03:14:49 +0530

    Add Favicon and useTitle

---
+++ src/Hooks/useTitle.js
@@ -0,0 +1,10 @@
+import { useEffect } from 'react';
+export function useTitle({openFood, orders}) {
+    useEffect(() => {
+        if(openFood) {
+            document.title = openFood.name;
+        } else {
+            document.title = orders.length === 0 ? `Pizza's Anyone` : `[${orders.length}] items in your order`;
+        }
+    })
+}
